d79e4e29fdd86eef11ea82b69cae38c55a46b0f2
Laura Padilla <l_gerevini@hotmail.com>
2019-11-19 15:38:57 -0300

    criptografia senha com bcryptjs

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,14 +3,16 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
-    try {
-        const user = await User.create(req.body)
-        .then(result => {
-            console.log(result);
+    const { email } = req.body;
 
-            return res.send({ usuario: result });
-        })
-        .catch(err => console.log(err));
+    try {
+        if(await User.findOne({ email }))
+            return res.status(400).send({ error: 'User already exists' });
+
+        const user = await User.create(req.body);
+
+        user.password = undefined;
+        return res.send({ user });
         
     } catch (err){
         return res.status(400).send({ error: 'Registration failed' });
